77120f65d531db36ddaf4596792dfe2ba97ccbfa
gdfaubion <gdfaubion@gmail.com>
2014-03-26 18:40:58 -0500

    Changed a link in my navbar

--- app/views/Layouts/master.blade.php
+++ app/views/Layouts/master.blade.php
@@ -16,7 +16,7 @@
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	      </button>
-	      <a class="navbar-brand" href="{{{ action('HomeController@showHome') }}}">| Grace Faubion |</a>
+	      <a class="navbar-brand" href="{{{ action('PostsController@index') }}}">| Grace Faubion |</a>
 	    </div>
 	      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
