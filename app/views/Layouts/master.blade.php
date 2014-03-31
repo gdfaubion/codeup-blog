a7883952c833f33ef4d23f38567e5e9705a22eb6
gdfaubion <gdfaubion@gmail.com>
2014-03-30 21:43:03 -0500

    commit

--- app/views/Layouts/master.blade.php
+++ app/views/Layouts/master.blade.php
@@ -5,26 +5,25 @@
     <script src="/js/jquery.js"></script>
     <script src="bootbox.min.js"></script>
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-	<link rel="stylesheet" href="/css/flatly-bootstrap.css">
-	<link rel="stylesheet" href="/css/resume.css">
+  <link rel="stylesheet" href="/css/flatly-bootstrap.css">
+  <link rel="stylesheet" href="/css/resume.css">
 </head>
 <body>
     <nav class="navbar navbar-default navbar-static-top" role="navigation">
-	  <div class="container">
-	  	  <div class="navbar-header">
-	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <a class="navbar-brand" href="{{{ action('PostsController@index') }}}">| Grace Faubion |</a>
-	    </div>
-	      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="container">
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="{{{ action('PostsController@index') }}}">| Grace Faubion |</a>
+      </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="{{ Request::is('Resume') ? 'active' : '' }}"><a href="{{{ action('HomeController@showResume') }}}">Resume</a></li>
-        <li class="{{ Request::is('Portfolio') ? 'active' : '' }}"><a class="nav-text" href="{{{ action('HomeController@showPortfolio') }}}">Portfolio</a></li>
-        <li class="{{ Request::is('Blog') ? 'active' : '' }}"><a class="nav-text" href="{{{ action('PostsController@index') }}}">Blog</a></li>
+        <li class="{{ Request::is('resume') ? 'active' : '' }}"><a href="{{{ action('HomeController@showResume') }}}">Resume</a></li>
+        <li class="{{ Request::is('portfolio') ? 'active' : '' }}"><a class="nav-text" href="{{{ action('HomeController@showPortfolio') }}}">Portfolio</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
           <ul class="dropdown-menu">
@@ -38,15 +37,15 @@
           </ul>
         </li>
       </ul>
-	  </div>
-	</nav>
+    </div>
+  </nav>
    @if (Session::has('successMessage'))
     <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
     @endif
     @if (Session::has('errorMessage'))
         <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
     @endif
-	@yield('content')
+  @yield('content')
 
   @yield('bottom-script')
   <script type="text/javascript">
@@ -54,6 +53,6 @@
       $('.alert-danger').fadeOut(3000);
   </script>
 </body>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
 </html>
