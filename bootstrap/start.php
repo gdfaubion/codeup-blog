525f6529d6a391ed6cab9c9c55d5710763884759
gdfaubion <gdfaubion@gmail.com>
2014-03-31 09:09:12 -0500

    entered machine name

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('Jessica-Harriss-MacBook-Air'),
 	'production' => array('tunnel.pagodabox.com')
 
 ));
